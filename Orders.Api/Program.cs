24e70f0719007efb961c0700127541e376c3ae56
UmerMalik298 <umer23672@gmail.com>
2026-02-18 15:30:39 +0500

    call one service from another

--- Orders.Api/Program.cs
+++ Orders.Api/Program.cs
@@ -13,6 +13,22 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 
+
+var productsBaseUrl = builder.Configuration["Services:Products"];
+
+app.MapGet("/details", async () =>
+{
+    using var http = new HttpClient();
+    var productResponse = await http.GetStringAsync($"{productsBaseUrl}/ping");
+
+    return new
+    {
+        OrderId = 123,
+        ProductServiceResponse = productResponse
+    };
+});
+
+
 app.MapGet("/ping", () => "Orders service OK");
 //app.MapGet("/orders/ping", () => "Orders service OK");
 
